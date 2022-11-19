72f404a18ba80acda4dcebe896015bd93d652e7c
Sol Peloc <solpeloc@gmail.com>
2022-11-19 11:42:40 -0300

    nuevo

---
+++ src/components/header/styles.js
@@ -0,0 +1,20 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+
+  container:{
+    backgroundColor:"#adb5bd",
+    height:100,
+    justifyContent:"center",
+    alignItems:"center",
+    paddingTop:20,
+
+  },
+title:{
+   fontSize: 20,
+   fontWeight:"bold",
+  
+
+}
+
+})
